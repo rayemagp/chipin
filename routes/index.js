ff0209fce0867ab1124e8f791785d4d2b5024793
Rayen Magpantay <raye.magp@gmail.com>
2015-12-11 15:16:02 +1100

    //TODO: Multi-level progress bar //TODO: Input amount functionality

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.locals.amount = req.query.amount;
-  res.render('index', { title: req.query.amount });
+  var max = 200;
+  var amount = (req.query.amount/max)*100;
+  res.locals.amount = amount;
+  res.locals.raised = req.query.amount;
+
+  res.render('index', {
+      title: req.query.amount,
+  });
 
 });
 
